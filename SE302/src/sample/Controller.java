a8dfcc7ca460f8f78ec85b62f3385902f3494428
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2018-12-18 16:59:47 +0300

    Edit

--- SE302/src/sample/Controller.java
+++ SE302/src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class Controller {
+
+
+    @FXML
+    private BorderPane borderPane;
+
+
+    public void showAddCollectionDialog() {
+
+        System.out.println("Collection eklemeye ait Dialog Pane ayarlanacak");
+        Dialog<ButtonType> dialog = new Dialog<>();
+
+    }
+
+    public void showEditCollectionDialog() {
+
+        System.out.println("Collection editlemeye ait Dialog Pane ayarlanacak");
+    }
+
+    public void showAddItemDialog() {
+        System.out.println("Belirtilen collectiona ait item ekleme işleme gerçekleştirilecek.");
+
+
+    }
+    public void showEditItemDialog(){
+        System.out.println("Belirtilen collectiona ait item editleme işleme gerçekleştirilecek.");
+
+    }
 }
