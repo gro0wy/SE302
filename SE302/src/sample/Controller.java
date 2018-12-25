a385d1695bb232b50fdcee4ec623498b057280f0
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2018-12-25 21:05:59 +0300

    showAddItemDialog organized according to database requests

--- SE302/src/sample/Controller.java
+++ SE302/src/sample/Controller.java
@@ -1,15 +1,16 @@
 package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 
@@ -101,7 +102,91 @@ public class Controller {
          */
 
         System.out.println("Item eklemeye ait Dialog Pane ayarlanacak");
+        if(collectionListView.getSelectionModel().getSelectedItem() == null){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Select a collection!");
+            alert.setContentText("Something went wrong");
+
+            alert.showAndWait();
+        }else{
+
+
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.initOwner(mainPanel.getScene().getWindow());
+            DialogPane itemDialogPane = new DialogPane();
+            dialog.setTitle("Add item");
+
+            //Bize burdan liste gelmesi gerekiyor (databaseden seçili collectiona göre) column nameleri mesela (Book'a ait BookName, BookPageNumber, BookAuthor bunların
+            //bir listede gelmesi lazım
+            ArrayList<String> sample = new ArrayList<>();
+/*
+            sample.add("Name");
+            sample.add("Age");
+            sample.add("page number");
+            sample.add("year");
+*/
+            VBox vBox = new VBox();
+            TextField[] textFields = new TextField[sample.size()];
+            Label[] labelList = new Label[sample.size()];
+
+            for(int i=0; i<sample.size(); i++){
+                labelList[i] = new Label(sample.get(i));
+                textFields[i] = new TextField();
+
+                vBox.getChildren().addAll(labelList[i], textFields[i]);
+            }
+
+            Button addItem = new Button("Add Item");
+            vBox.getChildren().add(addItem);
+            vBox.setSpacing(10);
+
+            ArrayList<String> userInputs = new ArrayList<>(); //User inputlarını bir yerde tutuyoruz
+            //Userdan inputları alıyoruz
+
+
+            try {
+                addItem.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        for (int i = 0; i < sample.size(); i++) {
+                            userInputs.add(textFields[i].getText());
+
+
+                        }
+
+                        // insert sql methodu çağırılacak ve userInputs List parametre olarak gönderilecek. Kolon sırasına göre inputlar insert edilecek.
+                        //methodsql(userInputs); //bu sınıf yazılırken içinde try catch kullanılmayacak hata bu sınıfı çağıran sınafa throw edilecek
+
+                           /* for (int j =0;j<sample.size();j++){
+                                System.out.println(sample.get(j)+" = " +userInputs.get(j));
+                            }*/
+                    }
+
+                });
+
+            } catch (Exception e) {
+
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setHeaderText("Look, a Warning Dialog");
+                alert.setContentText("Something went wrong!");
+
+                alert.showAndWait();
+
+            }
+
+            itemDialogPane.setContent(vBox);
+            dialog.getDialogPane().setContent(itemDialogPane);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+            dialog.showAndWait();
+
+        }
+
+
     }
+
 
     @FXML
     public void showEditItemDialog () {
